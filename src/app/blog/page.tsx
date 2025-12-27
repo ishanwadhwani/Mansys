6c86c82dc0cee0b87b9cfe151a0913ddaec7e67e
ishanwadhwani <ishan.209303191@muj.manipal.edu>
2025-12-27 23:42:36 +0530

    validate

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -67,6 +67,7 @@ export default async function BlogListingPage() {
         {/* Blog Grid Section */}
         <section className="py-16 md:py-24">
           <div className="container mx-auto px-4">
+            <h2>THIS IS A TEST MESSAGE TO VALIDATE</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-7xl mx-auto">
               {posts.map((post) => (
                 <Link
